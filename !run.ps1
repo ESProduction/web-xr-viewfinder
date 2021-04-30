a2fcf22108ab588c0176b4a20bc225d5e3e36ab5
ESProduction <production@elarasysems.com>
2021-04-30 15:01:14 -0700

    IB submitting changes to mesh

--- !run.ps1
+++ !run.ps1
@@ -1,2 +1,19 @@
+taskkill /F /IM "chrome.exe"
+Start-Sleep -Seconds 2
+$Items = @('Archived History',
+            'Cache\*',
+            'Cookies',
+            'History',
+            'Login Data',
+            'Top Sites',
+            'Visited Links',
+            'Web Data')
+$Folder = "$($env:LOCALAPPDATA)\Google\Chrome\User Data\Default"
+$Items | % { 
+    if (Test-Path "$Folder\$_") {
+        Remove-Item "$Folder\$_" 
+    }
+}
+
 Start-Process http://127.0.0.1:8080
 cmd.exe /c "http-server"
